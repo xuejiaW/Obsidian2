7e3af9005a6cd9aae8b45d77fb6332340e8e5984
XueJia Wang <wxjwangxuejia@outlook.com>
2024-04-23 20:41:33 +0800

    feat: make configuration to AppData path
    Signed-off-by: XueJia Wang <wxjwangxuejia@outlook.com>

--- Obsidian2/Configuration/ConfigurationMgr.cs
+++ Obsidian2/Configuration/ConfigurationMgr.cs
@@ -1,17 +1,21 @@
-﻿using Newtonsoft.Json;
+﻿using System.Reflection;
+using Newtonsoft.Json;
 
 namespace Obsidian2;
 
 public static class ConfigurationMgr
 {
     private static string s_ConfigurationPath = null;
-
     public static Configuration configuration { get; private set; }
 
     static ConfigurationMgr()
     {
-        string dir = Path.Join(Directory.GetCurrentDirectory(), "Resources\\");
-        s_ConfigurationPath = Path.Join(dir, "configuration.json");
+        if (!Directory.Exists(configurationPath))
+            Directory.CreateDirectory(configurationPath);
+
+        s_ConfigurationPath = Path.Join(configurationPath, "configuration.json");
+        if (!File.Exists(s_ConfigurationPath)) CreateInitialConfiguration();
+
         configuration = Load();
     }
 
@@ -24,5 +28,30 @@ public static class ConfigurationMgr
     {
         string json = JsonConvert.SerializeObject(configuration);
         File.WriteAllText(s_ConfigurationPath, json);
+    }
+
+    private static void CreateInitialConfiguration()
+    {
+        using Stream stream = Assembly.GetExecutingAssembly()
+                                      .GetManifestResourceStream("Obsidian2.Resources.configuration.json");
+        using FileStream fileStream = File.Create(s_ConfigurationPath);
+        stream.CopyTo(fileStream);
+        Console.WriteLine(s_ConfigurationPath);
+    }
+
+    private static string configurationPath
+    {
+        get
+        {
+            string folder = Environment.OSVersion.Platform == PlatformID.Unix
+                ? Path.Combine(GetEnvironmentVariable("HOME"), ".config", "Obsidian2")
+                : Path.Combine(GetEnvironmentVariable("USERPROFILE"), "APPData", "Local", "Obsidian2");
+            return folder;
+
+            string GetEnvironmentVariable(string key)
+            {
+                return Environment.GetEnvironmentVariable(key) ?? string.Empty;
+            }
+        }
     }
 }
