fe5b12b141186cf739b51d21bbf78ae51700a615
XueJia Wang <wxjwangxuejia@outlook.com>
2025-06-22 19:51:13 +0800

    feat: add github related configuration for set command
    Signed-off-by: XueJia Wang <wxjwangxuejia@outlook.com>

--- Obsidian2/Command/SetCommand.cs
+++ Obsidian2/Command/SetCommand.cs
@@ -9,6 +9,11 @@ public static class SetCommand
         var setCommand = new Command("set", "Set Configuration");
         setCommand.AddCommand(CreateSetHexoPostsDirCmd());
         setCommand.AddCommand(CreateSetObsidianVaultDirCmd());
+        setCommand.AddCommand(CreateSetGitHubTokenCmd());
+        setCommand.AddCommand(CreateSetGitHubOwnerCmd());
+        setCommand.AddCommand(CreateSetGitHubRepoCmd());
+        setCommand.AddCommand(CreateSetGitHubBranchCmd());
+        setCommand.AddCommand(CreateSetGitHubImagePathCmd());
         return setCommand;
     }
 
@@ -44,4 +49,85 @@ public static class SetCommand
             ConfigurationMgr.Save();
         }
     }
+
+    private static Command CreateSetGitHubTokenCmd()
+    {
+        var setGitHubTokenCmd = new Command("github-token", "Set GitHub Personal Access Token");
+        var tokenArg = new Argument<string>();
+        setGitHubTokenCmd.AddArgument(tokenArg);
+        setGitHubTokenCmd.SetHandler(SetGitHubToken, tokenArg);
+        return setGitHubTokenCmd;
+
+        void SetGitHubToken(string token)
+        {
+            ConfigurationMgr.configuration.GitHub.PersonalAccessToken = token;
+            ConfigurationMgr.Save();
+            Console.WriteLine("GitHub Personal Access Token has been set.");
+        }
+    }
+
+    private static Command CreateSetGitHubOwnerCmd()
+    {
+        var setGitHubOwnerCmd = new Command("github-owner", "Set GitHub repository owner");
+        var ownerArg = new Argument<string>();
+        setGitHubOwnerCmd.AddArgument(ownerArg);
+        setGitHubOwnerCmd.SetHandler(SetGitHubOwner, ownerArg);
+        return setGitHubOwnerCmd;
+
+        void SetGitHubOwner(string owner)
+        {
+            ConfigurationMgr.configuration.GitHub.RepoOwner = owner;
+            ConfigurationMgr.Save();
+            Console.WriteLine("GitHub repository owner has been set.");
+        }
+    }
+
+    private static Command CreateSetGitHubRepoCmd()
+    {
+        var setGitHubRepoCmd = new Command("github-repo", "Set GitHub repository name");
+        var repoArg = new Argument<string>();
+        setGitHubRepoCmd.AddArgument(repoArg);
+        setGitHubRepoCmd.SetHandler(SetGitHubRepo, repoArg);
+        return setGitHubRepoCmd;
+
+        void SetGitHubRepo(string repo)
+        {
+            ConfigurationMgr.configuration.GitHub.RepoName = repo;
+            ConfigurationMgr.Save();
+            Console.WriteLine("GitHub repository name has been set.");
+        }
+    }
+
+    private static Command CreateSetGitHubBranchCmd()
+    {
+        var setGitHubBranchCmd = new Command("github-branch", "Set GitHub repository branch");
+        var branchArg = new Argument<string>();
+        setGitHubBranchCmd.AddArgument(branchArg);
+        setGitHubBranchCmd.SetHandler(SetGitHubBranch, branchArg);
+        return setGitHubBranchCmd;
+
+        void SetGitHubBranch(string branch)
+        {
+            ConfigurationMgr.configuration.GitHub.BranchName = branch;
+            ConfigurationMgr.Save();
+            Console.WriteLine("GitHub repository branch has been set.");
+        }
+    }
+
+    private static Command CreateSetGitHubImagePathCmd()
+    {
+        var setGitHubImagePathCmd = new Command("github-image-path", "Set base path for images in GitHub repository");
+        var pathArg = new Argument<string>();
+        setGitHubImagePathCmd.AddArgument(pathArg);
+        setGitHubImagePathCmd.SetHandler(SetGitHubImagePath, pathArg);
+        return setGitHubImagePathCmd;
+
+        void SetGitHubImagePath(string path)
+        {
+            ConfigurationMgr.configuration.GitHub.ImageBasePath = path;
+            ConfigurationMgr.Save();
+            Console.WriteLine("GitHub image base path has been set.");
+        }
+    }
 }
+
